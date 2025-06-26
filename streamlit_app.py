c0eceb419ebd86c92ab49cc984015600ac18cc64
sandeepanjat <sandeepanjat@gmail.com>
2025-06-26 17:15:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(
 
 
 name_on_order = st.text_input("Name of Smoothie")
-#st.write("The name of your Smoothie will be:", name_on_order)
+st.write("The name of your Smoothie will be:", name_on_order)
 
 cnx= st.connection("snowflake")
 session = cnx.session()
@@ -41,7 +41,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen +' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
